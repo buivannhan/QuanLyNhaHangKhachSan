1513cac8d8fec56809e57e72a75a600fd51ac8b3
buivannhan <48819930+buivannhan@users.noreply.github.com>
2019-03-22 13:48:12 +0700

    Tao project QLNHKS
    Tạo được Form Main

---
+++ QuanLyNhaHangKhachSan/QuanLyNhaHangKhachSan/FrmMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace QuanLyNhaHangKhachSan
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(34, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phần mềm hỗ trợ Quản Lý Nhà Hàng Khách Sạn";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 316);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Quản Lý Nhà Hàng Khách Sạn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
